0b48c36d16ab2c28614ea936631122b2fce3867d
DavidMPN <david.manuel27@hotmail.com>
2021-08-24 11:40:59 -0300

    Creating next project

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+// Quem não inventa cópia.
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello world</h1>
+    </div>
+  )
+}

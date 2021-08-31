ef9e59d9b05885ff8ab3f08c2986d24206584a22
Joelson Ferreira <joelsonferreira@id.uff.br>
2021-08-31 15:44:02 -0300

    update room page to receive api data

--- pages/[room].jsx
+++ pages/[room].jsx
@@ -8,30 +8,64 @@ import Options from '../components/Options';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SettingsMenu from '../components/SettingsMenu'
+import axios from 'axios';
+import useSWR from 'swr';
 
-export default function Home({ room }) {
+function usePlayer(room) {
+  const { data, error } = useSWR('api/room/game', async (url) => {
+    const response = await axios.post(url, { roomname: room })
+    return response.data;
+  }, { refreshInterval: 1000 });
 
-  return (
-    <div className={styles.container}>
+  return {
+    data: data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
 
-      <SettingsMenu/>
+export default function Room({ room }) {
 
-      <Header/>
+  const { data, isLoading } = usePlayer(room);
 
-      <main>
-        <Question/>
-        <Options/>
-      </main>
-    
-      <Footer/>
-    </div>
-  )
+  console.log(data);
+
+  if(isLoading) {
+    return <h1>Carregando...</h1>
+  }
+
+  if(data.status) {
+    if(data.isStarted) {
+      return (
+        <div className={styles.container}>
+
+          <SettingsMenu/>
+
+          <Header/>
+
+          <main>
+            <Question/>
+            <Options/>
+          </main>
+        
+          <Footer/>
+        </div>
+      )    
+    }
+    return (
+      <ul>
+        {data.players.map(player => <li key={player.id}>{player.name}</li>)}
+      </ul>
+    );
+  }
+
+  return <h1>Você não pode entrar nessa sala</h1>
 }
 
 export async function getServerSideProps({ params }) {
   return {
     props: {
       room: params.room
-    }, // will be passed to the page component as props
+    }
   }
 }

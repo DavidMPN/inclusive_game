04766c63680a3755b3c95528fed18553afed67be
Joelson Ferreira <joelsonferreira@id.uff.br>
2021-08-31 16:11:48 -0300

    fix api/room/game route: when room is not found

--- pages/api/room/game.js
+++ pages/api/room/game.js
@@ -4,12 +4,15 @@ import withSession from "../../../lib/session";
 export default withSession(async (req, res) => {
     const { roomname } = req.body;
 
-    const room    = rooms.find(r => r.name == roomname);
-    const player  = req.session.get("player");
-    const auth    = room.players.find(p => p.id == player.id);
+    const room = rooms.find(r => r.name == roomname);
 
-    if(player && auth) {
-        return res.json({ status: true, players: room.players, isStarted: room.isStarted });
+    if(room) {
+        const player = req.session.get("player");
+        const auth   = room.players.find(p => p.id == player.id);
+    
+        if(player && auth) {
+            return res.json({ status: true, players: room.players, isStarted: room.isStarted });
+        }
     }
 
     return res.json({ status: false, players: [], isStarted: false });
